46217e5605ced8e4f6f051636f3a16c15ec8ec37
Jens Neuhalfen <jens@neuhalfen.name>
2016-11-27 20:24:52 +0100

    Add failing cmocka test

--- tests/test_example.c
+++ tests/test_example.c
@@ -9,9 +9,16 @@ static void null_test_success(void **state) {
     (void) state; /* unused */
 }
 
+/* A test case that does nothing and succeeds. */
+static void null_test_fail(void **state) {
+    (void) state; /* unused */
+    assert_string_equal("I should be","this");
+}
+
 int main(void) {
     const struct CMUnitTest tests[] = {
             cmocka_unit_test(null_test_success),
+            cmocka_unit_test(null_test_fail),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
