870ed609ed6541262ee0a4149af1a9e7117f5590
Jens Neuhalfen <jens@neuhalfen.name>
2016-11-28 06:51:47 +0100

    Fix 'inlining failed in call to always_inline 'delay_ms': function body not available'
    Inlining in (avr-)gcc is only possible when the function body is defined in the header file.

--- device/include/device.h
+++ device/include/device.h
@@ -3,6 +3,6 @@
 
 #pragma once
 
-void show_column(uint8_t column) __attribute__((always_inline));
+void show_column(uint8_t column);
 
-void delay_ms(double ms) __attribute__((always_inline));
+void delay_ms(double ms);
