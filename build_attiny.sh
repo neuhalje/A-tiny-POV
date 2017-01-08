994173a139c5415f952891d6650606210e39ef32
Jens Neuhalfen <jens@neuhalfen.name>
2017-01-08 18:26:20 +0100

    Set AVR fuses in build

--- build_attiny.sh
+++ build_attiny.sh
@@ -7,8 +7,8 @@ UPLOAD_PORT=/dev/spidev0.0
 AVR_PROGRAMMER=linuxspi
 AVR_UPLOADTOOL_OPTIONS="-b 10000"
 
-AVR_L_FUSE=
-AVR_H_FUSE=
+AVR_L_FUSE=0xE2
+AVR_H_FUSE=0xDF
 
 #
 ##### End of configuration
@@ -32,6 +32,7 @@ cmake -DCMAKE_TOOLCHAIN_FILE="${CMAKE_AVR_FILE}" \
     -DTARGET_CONFIGURATION=attiny85 -DAVR_MCU=attiny85 \
     -DAVR_PROGRAMMER=${AVR_PROGRAMMER} -DAVR_UPLOADTOOL_PORT=${UPLOAD_PORT} \
     -DAVR_UPLOADTOOL_OPTIONS="${AVR_UPLOADTOOL_OPTIONS}" \
+    -DAVR_L_FUSE="${AVR_L_FUSE}" -DAVR_H_FUSE="${AVR_H_FUSE}" \
     "$SRC_DIR"
 
-make && sudo make upload_pov
+make && sudo make set_fuses upload_pov
